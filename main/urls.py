a38490a98e00e43c0ece9a6f37c040197600986e
Macca Williams <mackenzie050598@gmail.com>
2025-01-02 19:41:30 +0000

    added some html to the templates

--- main/urls.py
+++ main/urls.py
@@ -1,3 +1,11 @@
+from django.urls import path
+from . import views
+
 urlpatterns = [
+    path("", views.home, name="home"),
+    path("home/", views.home, name="home"),
+    path("contact/", views.contact, name="contact"),
+    # Below is what's known as a path variable, this allows you to get an individual id for say the project
+    path("project/<int:id>/", views.project, name="project")
 ]
 
